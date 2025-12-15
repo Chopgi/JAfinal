44b70a6f04378aa28b565a6372db590fbe157ff6
Jay <jaiden.moon@gmail.com>
2025-12-15 10:33:21 -0600

    test

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -90,7 +90,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(360, 210);
+            this.label1.Location = new System.Drawing.Point(548, 123);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 31);
             this.label1.TabIndex = 12;
