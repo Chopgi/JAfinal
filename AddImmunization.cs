0378788b1876ac9b13be4d47b2c4c56a307b1787
Chopgi <chop10009@gmail.com>
2025-12-17 17:35:38 -0600

    Committing pending changes

---
+++ AddImmunization.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student2
+{
+    public partial class AddImmunization : Form
+    {
+        public AddImmunization()
+        {
+            InitializeComponent();
+        }
+
+        private void AddImmunization_Load(object sender, EventArgs e)
+        {
+            dateBox.Text = DateTime.Now.ToShortDateString();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- MedicationHistory.Designer.cs
+++ MedicationHistory.Designer.cs
@@ -36,14 +36,13 @@
             this.visitGMHbut = new System.Windows.Forms.Button();
             this.nameAgeLabel = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.visitPDbut = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // deleteButMH
             // 
             this.deleteButMH.Location = new System.Drawing.Point(430, 337);
-            this.deleteButMH.Margin = new System.Windows.Forms.Padding(2);
+            this.deleteButMH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.deleteButMH.Name = "deleteButMH";
             this.deleteButMH.Size = new System.Drawing.Size(56, 19);
             this.deleteButMH.TabIndex = 9;
@@ -53,7 +52,7 @@
             // undoButMH
             // 
             this.undoButMH.Location = new System.Drawing.Point(351, 337);
-            this.undoButMH.Margin = new System.Windows.Forms.Padding(2);
+            this.undoButMH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.undoButMH.Name = "undoButMH";
             this.undoButMH.Size = new System.Drawing.Size(56, 19);
             this.undoButMH.TabIndex = 8;
@@ -63,7 +62,7 @@
             // modifyButMH
             // 
             this.modifyButMH.Location = new System.Drawing.Point(194, 337);
-            this.modifyButMH.Margin = new System.Windows.Forms.Padding(2);
+            this.modifyButMH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.modifyButMH.Name = "modifyButMH";
             this.modifyButMH.Size = new System.Drawing.Size(56, 19);
             this.modifyButMH.TabIndex = 7;
@@ -72,8 +71,8 @@
             // 
             // addButMH
             // 
-            this.addButMH.Location = new System.Drawing.Point(114, 337);
-            this.addButMH.Margin = new System.Windows.Forms.Padding(2);
+            this.addButMH.Location = new System.Drawing.Point(115, 337);
+            this.addButMH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addButMH.Name = "addButMH";
             this.addButMH.Size = new System.Drawing.Size(56, 19);
             this.addButMH.TabIndex = 6;
@@ -82,8 +81,8 @@
             // 
             // saveButMH
             // 
-            this.saveButMH.Location = new System.Drawing.Point(272, 337);
-            this.saveButMH.Margin = new System.Windows.Forms.Padding(2);
+            this.saveButMH.Location = new System.Drawing.Point(273, 337);
+            this.saveButMH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.saveButMH.Name = "saveButMH";
             this.saveButMH.Size = new System.Drawing.Size(56, 19);
             this.saveButMH.TabIndex = 5;
@@ -93,7 +92,7 @@
             // visitGMHbut
             // 
             this.visitGMHbut.Location = new System.Drawing.Point(520, 185);
-            this.visitGMHbut.Margin = new System.Windows.Forms.Padding(2);
+            this.visitGMHbut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.visitGMHbut.Name = "visitGMHbut";
             this.visitGMHbut.Size = new System.Drawing.Size(71, 50);
             this.visitGMHbut.TabIndex = 15;
@@ -104,12 +103,10 @@
             // nameAgeLabel
             // 
             this.nameAgeLabel.AutoSize = true;
-            this.nameAgeLabel.BackColor = System.Drawing.Color.Gold;
-            this.nameAgeLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameAgeLabel.BackColor = System.Drawing.Color.Yellow;
             this.nameAgeLabel.Location = new System.Drawing.Point(9, 7);
-            this.nameAgeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nameAgeLabel.Name = "nameAgeLabel";
-            this.nameAgeLabel.Size = new System.Drawing.Size(289, 29);
+            this.nameAgeLabel.Size = new System.Drawing.Size(150, 13);
             this.nameAgeLabel.TabIndex = 16;
             this.nameAgeLabel.Text = "PatientFname, Lname, Age";
             // 
@@ -117,29 +114,21 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(88, 91);
+            this.dataGridView1.Location = new System.Drawing.Point(97, 82);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(398, 139);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(406, 202);
             this.dataGridView1.TabIndex = 17;
-            // 
-            // visitPDbut
-            // 
-            this.visitPDbut.Location = new System.Drawing.Point(520, 131);
-            this.visitPDbut.Margin = new System.Windows.Forms.Padding(2);
-            this.visitPDbut.Name = "visitPDbut";
-            this.visitPDbut.Size = new System.Drawing.Size(71, 50);
-            this.visitPDbut.TabIndex = 18;
-            this.visitPDbut.Text = "Patient Demographics";
-            this.visitPDbut.UseVisualStyleBackColor = true;
-            this.visitPDbut.Click += new System.EventHandler(this.visitPDbut_Click);
             // 
             // MedicationHistory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.visitPDbut);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.nameAgeLabel);
             this.Controls.Add(this.visitGMHbut);
@@ -148,7 +137,7 @@
             this.Controls.Add(this.modifyButMH);
             this.Controls.Add(this.addButMH);
             this.Controls.Add(this.saveButMH);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MedicationHistory";
             this.Text = "MedHistory";
             this.Load += new System.EventHandler(this.MedicationHistory_Load);
@@ -168,6 +157,5 @@
         private System.Windows.Forms.Button visitGMHbut;
         private System.Windows.Forms.Label nameAgeLabel;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button visitPDbut;
     }
 }
